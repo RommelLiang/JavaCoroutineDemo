9252410faeba9debfb06304ab09a06da0104995e
Karl <lzz931208@yeah.net>
2022-02-24 13:43:38 +0800

    commit
    注释

--- src/eg/KotlinEqualsJava.java
+++ src/eg/KotlinEqualsJava.java
@@ -3,6 +3,12 @@ package eg;
 public class KotlinEqualsJava {
 
     void demo() {
+        /**首先创建一个匿名内部类
+         * 对应挂起函数:
+            suspend {
+                println("A----------------${Thread.currentThread().name}")
+            }
+         **/
         SuspendLambda<String> suspendLambda = new SuspendLambda<String>(null) {
             @Override
             public BaseContinuationImpl<String> create(Continuation<String> continuation) {
@@ -12,6 +18,7 @@ public class KotlinEqualsJava {
 
                     }
 
+                    //实现create方法
                     @Override
                     public BaseContinuationImpl create(Continuation<String> continuation) {
                         return new SuspendLambda<String>(continuation) {
@@ -35,16 +42,29 @@ public class KotlinEqualsJava {
 
             }
         };
+        /**再创建一个匿名内部类
+         * 对应
+         object : Continuation<Unit> {
+            override val context: CoroutineContext
+            get() = EmptyCoroutineContext
+
+             override fun resumeWith(result: Result<Unit>) {
+                println("B----------------${Thread.currentThread().name}")
+             }
+         }
+         **/
         Continuation<String> completion = new Continuation<>() {
             @Override
             public void resumeWith() {
 
             }
         };
-        BaseContinuationImpl coroutine = suspendLambda.createCoroutine(completion);
+        //接着就是调用createCoroutine方法
+        Continuation coroutine = suspendLambda.createCoroutine(completion);
         coroutine.resumeWith();
     }
 
+    //SuspendLambda用来描述匿名挂起函数的实现
     abstract class SuspendLambda<T> extends BaseContinuationImpl<T> {
         Continuation<T> mContinuation;
 
@@ -52,30 +72,34 @@ public class KotlinEqualsJava {
             mContinuation = continuation;
         }
 
-        public abstract BaseContinuationImpl create(Continuation<T> continuation);
 
-        public BaseContinuationImpl createCoroutine(Continuation<T> completion) {
+        public Continuation createCoroutine(Continuation<T> completion) {
+            //创建SafeContinuation
             return new SafeContinuation(createCoroutineUnintercepted(completion).intercepted());
         }
 
         public BaseContinuationImpl createCoroutineUnintercepted(Continuation<T> completion) {
+            //调用create，该方法在BaseContinuationImpl中定义
+            // 但是在demo中的匿名内部类中实现
             return create(completion);
         }
 
     }
 
     abstract class BaseContinuationImpl<T> extends Continuation<T> {
+        public abstract BaseContinuationImpl create(Continuation<T> continuation);
         BaseContinuationImpl<T> intercepted() {
             return this;
         }
     }
 
 
+    //假装它就是协程
     abstract class Continuation<T> {
         abstract void resumeWith();
     }
 
-    class SafeContinuation<T> extends BaseContinuationImpl<T> {
+    class SafeContinuation<T> extends Continuation<T> {
         public Continuation<T> delegate;
 
         public SafeContinuation(Continuation<T> completion) {
@@ -84,7 +108,8 @@ public class KotlinEqualsJava {
 
         @Override
         public void resumeWith() {
-
+            delegate.resumeWith();
         }
+
     }
 }
