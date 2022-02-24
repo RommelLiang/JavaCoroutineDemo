073c5989ac7a5c8eb2fca37cc43ddfa1d7863ec2
Karl <lzz931208@yeah.net>
2022-02-24 14:41:30 +0800

    commit
    创建启动流程

--- src/DemoRun.java
+++ src/DemoRun.java
@@ -3,25 +3,29 @@ import coroutine.SuspendAnonymous;
 
 public class DemoRun {
     public static void main(String[] args) {
-        CoroutineInterface completion = new CoroutineInterface() {
+        //创建匿名内部类(指代挂起函数)
+        SuspendAnonymous suspendAnonymous = new SuspendAnonymous(new CoroutineInterface() {
+            //创建匿名内部类，指代createCoroutine的Continuation参数
             @Override
             public void resumeWith() {
-                System.out.println("A-------------");
+                System.out.println("B-------------");
             }
-        };
-        SuspendAnonymous suspendAnonymous = new SuspendAnonymous(completion) {
-
+        }) {
+            //（挂起函数）实现resumeWith
             @Override
             public void resumeWith() {
                 invokeSuspend();
                 mCoroutineInterface.resumeWith();
             }
 
+            //（挂起函数）实现invokeSuspend
+            //用来指代挂起函数代码块
             public void invokeSuspend() {
-                System.out.println("B-------------");
+                System.out.println("A-------------");
             }
         };
         CoroutineInterface coroutineInterface = SuspendAnonymous.creatCoroutune(suspendAnonymous);
         coroutineInterface.resumeWith();
+        System.out.println("C-------------");
     }
 }
