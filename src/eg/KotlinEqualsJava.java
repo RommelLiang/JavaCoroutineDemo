956fcc65b0f3f70b93a839b8d7e1af3b4c5ba693
Karl <lzz931208@yeah.net>
2022-02-24 13:34:07 +0800

    commit
    KotlinEqualsJava

--- src/eg/KotlinEqualsJava.java
+++ src/eg/KotlinEqualsJava.java
@@ -2,21 +2,89 @@ package eg;
 
 public class KotlinEqualsJava {
 
-    class SuspendLambda<T> {
-        Continuation createCoroutine(Continuation<T> completion) {
-            return new SafeContinuation(completion);
+    void demo() {
+        SuspendLambda<String> suspendLambda = new SuspendLambda<String>(null) {
+            @Override
+            public BaseContinuationImpl<String> create(Continuation<String> continuation) {
+                return new SuspendLambda<String>(continuation) {
+                    @Override
+                    void resumeWith() {
+
+                    }
+
+                    @Override
+                    public BaseContinuationImpl create(Continuation<String> continuation) {
+                        return new SuspendLambda<String>(continuation) {
+
+                            @Override
+                            void resumeWith() {
+
+                            }
+
+                            @Override
+                            public BaseContinuationImpl create(Continuation<String> continuation) {
+                                return null;
+                            }
+                        };
+                    }
+                };
+            }
+
+            @Override
+            void resumeWith() {
+
+            }
+        };
+        Continuation<String> completion = new Continuation<>() {
+            @Override
+            public void resumeWith() {
+
+            }
+        };
+        BaseContinuationImpl coroutine = suspendLambda.createCoroutine(completion);
+        coroutine.resumeWith();
+    }
+
+    abstract class SuspendLambda<T> extends BaseContinuationImpl<T> {
+        Continuation<T> mContinuation;
+
+        public SuspendLambda(Continuation<T> continuation) {
+            mContinuation = continuation;
+        }
+
+        public abstract BaseContinuationImpl create(Continuation<T> continuation);
+
+        public BaseContinuationImpl createCoroutine(Continuation<T> completion) {
+            return new SafeContinuation(createCoroutineUnintercepted(completion).intercepted());
+        }
+
+        public BaseContinuationImpl createCoroutineUnintercepted(Continuation<T> completion) {
+            return create(completion);
+        }
+
+    }
+
+    abstract class BaseContinuationImpl<T> extends Continuation<T> {
+        BaseContinuationImpl<T> intercepted() {
+            return this;
         }
     }
 
 
-    interface Continuation<T> {
+    abstract class Continuation<T> {
+        abstract void resumeWith();
     }
 
-    class SafeContinuation<T> implements Continuation<T> {
+    class SafeContinuation<T> extends BaseContinuationImpl<T> {
         public Continuation<T> delegate;
 
         public SafeContinuation(Continuation<T> completion) {
             this.delegate = completion;
+        }
+
+        @Override
+        public void resumeWith() {
+
         }
     }
 }
