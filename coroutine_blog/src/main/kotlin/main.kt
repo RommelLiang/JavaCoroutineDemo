1dbaefdc06f2b4989b138432bd4fa6b69a77151f
Karl <lzz931208@yeah.net>
2022-02-25 13:34:38 +0800

    commit
    image

--- coroutine_blog/src/main/kotlin/main.kt
+++ coroutine_blog/src/main/kotlin/main.kt
@@ -1,16 +1,21 @@
 import kotlin.coroutines.*
 
 fun main() {
+    val coroutineContext =
+        FirstContext() + EmptyCoroutineContext + SecondContext() + ThirdContext() + FourthContext()
     val createCoroutine = suspend {
         println("A------------")
         "B"
     }.createCoroutine(object : Continuation<String> {
         override val context: CoroutineContext
-            get() = FirstContext()
+            get() = coroutineContext
 
         override fun resumeWith(result: Result<String>) {
             println("${result.getOrNull()}----------------")
             context[FirstContext]?.first()
+            context[SecondContext]?.second()
+            context[ThirdContext]?.third()
+            context[FourthContext]?.fourth()
         }
     })
     createCoroutine.resume(Unit)
@@ -18,28 +23,32 @@ fun main() {
 
 class FirstContext : AbstractCoroutineContextElement(Key) {
     companion object Key : CoroutineContext.Key<FirstContext>
-    fun first(){
+
+    fun first() {
         println("第一个上下文------------------")
     }
 }
 
 class SecondContext : AbstractCoroutineContextElement(Key) {
     companion object Key : CoroutineContext.Key<SecondContext>
-    fun second(){
+
+    fun second() {
         println("第二个上下文------------------")
     }
 }
 
 class ThirdContext : AbstractCoroutineContextElement(Key) {
     companion object Key : CoroutineContext.Key<ThirdContext>
-    fun third(){
+
+    fun third() {
         println("第三个上下文------------------")
     }
 }
 
 class FourthContext : AbstractCoroutineContextElement(Key) {
     companion object Key : CoroutineContext.Key<FourthContext>
-    fun fourth(){
+
+    fun fourth() {
         println("第四个上下文------------------")
     }
 }
